1add5e78aa483265774bc3dea21a6c98ce4a8705
Mike Ledger <eleventynine@gmail.com>
2013-07-07 17:42:28 +1000

    support empty arrays/lists; add helpers

--- src/Save.hs
+++ src/Save.hs
@@ -1,10 +1,24 @@
-module Save where
+{-# LANGUAGE OverloadedStrings #-}
+module Save 
+    ( Savable(..)
+    , Config
+    , Key, Type
+    , mkConfig
+    , conf
+    , textT, natT, listT, arrayT, pairT
+    , text, nat, list, pair
+    , lookupConf
+    , parseConfig
+    , readConfig
+    , formatConfig
+    , saveConfig
+    ) where
 import qualified Data.Map.Strict as M
 import qualified Data.Vector as V
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.Attoparsec.Text as A
-import Control.Monad.State
+import Control.Monad.Writer
 import Control.Applicative
 import Data.List
 
@@ -14,6 +28,13 @@ class Savable a where
 
 type Config = M.Map Key Value
 
+mkConfig :: Writer Config () -> Config
+mkConfig = execWriter
+
+conf :: T.Text -> Value -> Writer Config ()
+conf t v = tell $ 
+    M.singleton (Key (T.split (== '.') t) (valType v)) v
+
 data Type 
     = TN -- ^ natural number
     | TT -- ^ text
@@ -21,14 +42,35 @@ data Type
     | THL (V.Vector Type) -- ^ heterogenous list
   deriving (Show,Read,Eq,Ord)
 
-data Key  = Key [T.Text] !Type
+type Category = [T.Text]
+
+textT, natT :: Category -> Key
+textT c = Key c TT
+natT  c = Key c TN
+
+listT :: Category -> Type -> Key
+listT c x = Key c (TL x)
+
+arrayT :: Category -> [Type] -> Key
+arrayT c x = Key c (THL (V.fromList x))
+
+pairT :: Category -> Type -> Type -> Key
+pairT c x y = Key c (THL (V.fromList [x,y]))
+
+pair :: Value -> Value -> Value
+pair v t = L (V.fromList [v,t])
+
+text :: T.Text -> Value
+text = T
+
+nat :: Real a => a -> Value
+nat v = N (realToFrac v)
+
+list :: [Value] -> Value
+list = L . V.fromList
+
+data Key  = Key Category !Type
   deriving (Eq,Ord)
-
-typed :: T.Text -> Type -> Key
-typed = Key . T.split (== '.')
-
-(=:) :: Key -> Value -> (Key,Value)
-(=:) = (,)
 
 instance Show Key where
     show (Key ks _) = intercalate "." (map T.unpack ks)
@@ -38,18 +80,20 @@ data Value
     | T !T.Text
     | L !(V.Vector Value)
   deriving (Eq,Ord)
-
 instance Show Value where
     show (N n)  = show n
     show (T t)  = show t
-    show (L vs) = show (V.toList vs)
+    show (L vs) = "[" ++ intercalate " " (V.toList (V.map show vs)) ++ "]"
 
 valType :: Value -> Type
 valType (N _)  = TN
 valType (T _)  = TT
 valType (L vs) = 
     if V.all (\v -> Just v == vs V.!? 0) vs
-    then TL (valType (V.head vs))
+    then case vs V.!? 0 of
+        Just v -> TL (valType v)
+        _      -> THL V.empty
+        
     else THL (V.map valType vs)
 
 okType :: Value -> Type -> Bool
@@ -61,14 +105,14 @@ okType (L v) (TL t)   = case v V.!? 0 of
 okType (L l) (THL ts) = V.and (V.zipWith (\v t -> valType v == t) l ts)
 okType _     _        = False
 
-readValue :: Key -> Config -> Maybe Value
-readValue key@(Key _ type_) conf = do
+lookupConf :: Key -> Config -> Maybe Value
+lookupConf key@(Key _ type_) conf = do
     value <- M.lookup key conf
     guard (okType value type_)
     return value
 
 parseValue :: A.Parser Value
-parseValue = text <|> number <|> list
+parseValue = text <|> number <|> list <|> nil
   where
     chars   = mapM A.char
 
@@ -81,12 +125,15 @@ parseValue = text <|> number <|> list
             (A.char '"')
         return (T.pack (concat inside))
 
-    primitive = text <|> number
-
+    nil  = do
+        A.char '['
+        A.skipSpace
+        A.char ']'
+        return (L V.empty)
     list = do
         A.char '['
         vals <- A.manyTill 
-            (A.skipSpace *> primitive <* A.skipSpace)
+            (A.skipSpace *> parseValue <* A.skipSpace)
             (A.char ']') 
         return (L (V.fromList vals))
 
@@ -110,17 +157,30 @@ parseKeyValues types = M.fromList <$> A.many1' correctlyTypedKV
         (keys,val) <- parseKeyValue
         typ <- case M.lookup keys types of
                 Just typ' -> do
-                    guard (valType val == typ')
+                    guard (okType val typ')
                     return typ'
                 _         -> return (valType val)
         A.endOfLine
         return (Key keys typ, val)
 
-readConfig :: FilePath -> IO Config
-readConfig path = do
-    cfg <- T.readFile path
-    case A.parseOnly (parseKeyValues M.empty) cfg of
-        Right c -> return c
-        Left e  -> error e
+parseConfig :: T.Text -> Config
+parseConfig t = case A.parseOnly (parseKeyValues M.empty) t of
+    Right c -> c
+    Left e  -> error e
 
+readConfig :: FilePath -> IO Config
+readConfig path = parseConfig `fmap` T.readFile path
+
+saveConfig :: FilePath -> Config -> IO ()
+saveConfig path = T.writeFile path . formatConfig
+
+formatConfig :: Config -> T.Text
+formatConfig m = execWriter $ flip M.traverseWithKey m $ \(Key cat _) val -> do
+    str (T.intercalate "." cat)
+    str " = "
+    str (T.pack (show val))
+    ln
+  where
+    str = tell
+    ln  = tell "\n"
 
